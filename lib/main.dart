6c9f1b61e302710c3dfd8b20ae904db3024a7157
Yewooony <94075827+Yewooony@users.noreply.github.com>
2022-06-28 21:44:03 +0900

    json test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
-printInteger(int aNumber){
-  print('The number is $aNumber.');
+import 'dart:convert';
+
+void main() {
+  var jsonString = '''
+  [
+    {"score":40},
+    {"score":80}
+  ]
+  ''';
+
+  var scores = jsonDecode(jsonString);
+  print(scores is List);
+  var firstScore = scores[0];
+  print(firstScore is Map);
+  print(firstScore['score'] == 40);
 }
 
-main(){
-  var number = 42;
-  printInteger(number);
-}
